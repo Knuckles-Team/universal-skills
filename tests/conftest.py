f8948f1c3d8ebd052511857bc3b5dc46a61696fb
knucklessg1 <knucklessg1@gmail.com>
2026-05-17 01:35:11 -0500

    Isolate test databases

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,3 +7,12 @@ def pytest_configure(config):
         "markers",
         "concept(id): mark test with a concept ID for traceability",
     )
+
+import os
+import tempfile
+import atexit
+import shutil
+
+_test_db_dir = tempfile.mkdtemp(prefix="universal_skills_test_db_")
+os.environ["GRAPH_DB_PATH"] = os.path.join(_test_db_dir, "test_knowledge_graph.db")
+os.environ["AGENT_UTILITIES_TESTING"] = "true"
